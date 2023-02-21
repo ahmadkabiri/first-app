00a48a6ddc86835a54555c3d041dff8fd6814dc0
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-02-21 11:48:25 +0330

    about props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css"
+import Product from "./product/Product";
 function App() {
+
+
+  const allProducts = [
+    {id : 1, name : "react.js course" , price:200 , src:"logo512.png" , count:12 },
+    {id : 2, name : "node.js course" , price:176 , src:"index.png" , count:10 },
+    {id : 3, name : "js course" , price:243 , src:"js.png" , count:15 },
+
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Product {...allProducts[0]}></Product>
+      <Product {...allProducts[1]}></Product>
+      <Product {...allProducts[2]}></Product>
     </div>
   );
 }
---
+++ src/product/Product.js
@@ -0,0 +1,50 @@
+
+import "./Product.css"
+import React from 'react'
+
+export default function Product({name,price,src,count:number}) {
+    // console.log(props)
+
+    // let {name,price,src,count} = props
+    let count = "none"
+    return(
+        count && 
+        <div className='product'>
+        <h1>{name}</h1>
+        <h2>{number}</h2>
+        <img src={src} alt="" />
+        <div className='description'>
+            <p>{price}$</p>
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+    )
+
+
+    // console.log(props)
+    // if(props.count>0){
+
+    //     return (
+        //     <div className='product'>
+        //       <h1>{props.name}</h1>
+        //       <img src={props.src} alt="" />
+        //       <div className='description'>
+        //           <p>{props.price}$</p>
+        //           <button>ADD TO CART</button>
+        //       </div>
+        //   </div>
+    //     )
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
