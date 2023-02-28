9feeec272814dffec66c2f5c695dc572ce2b1746
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-02-28 10:29:05 +0330

    forms in react

--- src/App.js
+++ src/App.js
@@ -3,27 +3,53 @@ import User from './components/User/User' ;
 
 export default class App extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      username :'' ,
+      textareaValue : "" ,
+      isLogin : false 
+    }
 
-  state = {
-    users : [
-      {id:1 , name: "amin" , age : 22},
-      {id:2 , name: "amir" , age : 19},
-      {id:3 , name: "ali" , age : 23},
-      {id:4 , name: "babak" , age : 29},
-    ]
+    this.textareaHandler = this.textareaHandler.bind(this)
+
   }
 
+  usernameHandler (event) {
+    console.log(event)
+    this.setState({
+      username : event.nativeEvent.target.value
+    })
+  }
 
+  textareaHandler (event) {
+console.log(event)
+this.setState ({
+  textareaValue : event.nativeEvent.target.value
+})
+  }
+
+  isLoginHandler (event) {
+    console.log(event.nativeEvent.target.checked)
+    this.setState({
+      isLogin : event.nativeEvent.target.checked
+    })
+  }
+
+  
   render() {
   
     return (
-  <div>
-    {this.state.users.map((user) => (
-      <div key={user.id}>
-        <User {...user}></User>
-      </div>
-    ) )}
-  </div>
+       <div>
+        <h3>{this.state.username}</h3>
+            <div><input type="text" value={this.state.username} onChange={this.usernameHandler.bind(this)}/></div>
+            <div><textarea name="" id="" cols="30" rows="10" value={this.state.textareaValue} onChange={this.textareaHandler}></textarea></div>
+
+            <h3>{this.state.username}</h3>
+            <label htmlform = "login">Login</label>
+            <div><input id="login" type="checkbox"  checked={this.state.isLogin} onChange={this.isLoginHandler.bind(this)}/></div>
+            <div><textarea name="" id="" cols="30" rows="10" value={this.state.textareaValue} onChange={this.textareaHandler}></textarea></div>
+       </div>
     )
 
   }
