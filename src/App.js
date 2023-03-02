618481d010318b885ed85ec6daa36c33652d195b
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-03-02 10:36:37 +0330

    method as props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
-import Ticket from './components/Ticket/Ticket'
+import User from './User/User'
 
 export default class App extends Component {
+    constructor (props){
+        super(props)
+
+        this.state = {
+            users : [
+                {id:1 , name:"Mohmmad" },
+                {id:2 , name:"Amir" },
+                {id:3 , name:"Ali" },
+                {id:4 , name:"Amin" },
+
+            ]
+        }
+    }
+
+    userRemoveHandler (userId) {
+
+
+        let newUsers = [...this.state.users]
+        
+        let mainUserIndex = newUsers.findIndex(user => (
+            user.id === userId
+        ))
+        
+        newUsers.splice(mainUserIndex,1)
+        console.log(newUsers)
+
+        this.setState({
+            users : newUsers
+        })
+    }
     render() {
         return (
             <div>
-                <Ticket />
+                {this.state.users.map(user => (
+                    <User key={user.id} {...user} onRemove={this.userRemoveHandler.bind(this)}></User>
+                ))}
             </div>
         )
     }
