8fc7d092122b9a5405749b1ca349a48fe6a0122f
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-03-02 23:03:41 +0330

    bookList in js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import React, { Component } from 'react'
-import User from './User/User'
+import Header  from './components/Header/Header'
+import AddForm from './components/BookList/AddForm'
 
 export default class App extends Component {
-    constructor (props){
-        super(props)
 
-        this.state = {
-            users : [
-                {id:1 , name:"Mohmmad" },
-                {id:2 , name:"Amir" },
-                {id:3 , name:"Ali" },
-                {id:4 , name:"Amin" },
-
-            ]
-        }
-    }
-
-    userRemoveHandler (userId) {
-
-
-        let newUsers = [...this.state.users]
-        
-        let mainUserIndex = newUsers.findIndex(user => (
-            user.id === userId
-        ))
-        
-        newUsers.splice(mainUserIndex,1)
-        console.log(newUsers)
-
-        this.setState({
-            users : newUsers
-        })
-    }
     render() {
         return (
-            <div>
-                {this.state.users.map(user => (
-                    <User key={user.id} {...user} onRemove={this.userRemoveHandler.bind(this)}></User>
-                ))}
+            <div className='container mt-4'>
+                <Header></Header>
+                <AddForm></AddForm>
             </div>
         )
     }
