658fe29a7ca53307d8f52ee12bc873d3fe053731
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-03-03 16:27:11 +0330

    modal in react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,47 +2,32 @@ import React, { Component } from 'react'
 import Shop from './components/Shop/Shop'
 import './App.css'
 import { Alert } from 'react-bootstrap'
-import { Card , Button } from 'react-bootstrap'
+import { Card , Button ,Modal } from 'react-bootstrap'
 
 export default class App extends Component {
 
     state = {
-        showAlert : true
+        showModal : true
     }
 
     render() {
         return (
-            <div className='container mt-5'>
-                <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="Images/parsnaz.jpg" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-            آیا میخواهید سیبیل پر پشت تری داشته باشید؟
-        </Card.Text>
-        <Button variant="success">دکمتو بزن</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="Images/parsnaz.jpg" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-            آیا میخواهید سیبیل پر پشت تری داشته باشید؟
-        </Card.Text>
-        <Button variant="success">دکمتو بزن</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="Images/parsnaz.jpg" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-            آیا میخواهید سیبیل پر پشت تری داشته باشید؟
-        </Card.Text>
-        <Button variant="success">دکمتو بزن</Button>
-      </Card.Body>
-    </Card>
+            <div>
+                <Modal centered show={this.state.showModal} onHide={() => this.setState({showModal:false})}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>Modal body text goes here.</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => this.setState({showModal:false})}>Close</Button>
+                        <Button variant="primary">Save changes</Button>
+                    </Modal.Footer>
+                </Modal>
+                <Button onClick={() => this.setState({showModal:true})}>Reomve File</Button>
             </div>
         )
     }
