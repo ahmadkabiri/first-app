740b1a8d268a7ad732021de3b4ad8ae63152029a
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-03-03 15:38:57 +0330

    alert in react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
 import Shop from './components/Shop/Shop'
 import './App.css'
+import { Alert } from 'react-bootstrap'
 
 export default class App extends Component {
+
+    state = {
+        showAlert : true
+    }
 
     render() {
         return (
             <div>
-                <Shop />
+                <h1>Hello SabzLearn</h1>
+                <hr />
+                {/* <Alert variant="success">
+                    <Alert.Heading>Hey, nice to see you</Alert.Heading>
+                    <p>
+                        Aww yeah, you successfully read this important alert message. This
+                        example text is going to run a bit longer so that you can see how
+                        spacing within an alert works with this kind of content.
+                    </p>
+                    <hr />
+                    <p className="mb-0">
+                        Whenever you need to, be sure to use margin utilities to keep things
+                        nice and tidy.
+                    </p>
+                </Alert> */}
+                {this.state.showAlert && (
+                <Alert variant="danger" onClose={() => this.setState({showAlert:false})} dismissible>
+                    <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                    <p>
+                        Change this and that and try again. Duis mollis, est non commodo
+                        luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+                        Cras mattis consectetur purus sit amet fermentum.
+                    </p>
+                </Alert>
+                )}
+
+                <button onClick={()=>this.setState({showAlert:true})}>Show Alert</button>
             </div>
         )
     }
