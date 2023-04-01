5e8e6b8859a77e8e420496274cfa4696b3e89843
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-01 23:14:25 +0430

    quiz app

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Form.css";
 
-export default class App extends React.Component {
+export default class Form extends React.Component {
 
-    constructor(props) {
-        super(props)
+
+    constructor() {
+        super()
 
         this.state = {
             firstNameData: '',
@@ -15,92 +16,100 @@ export default class App extends React.Component {
 
             allValid: false
         }
+
+        this.submitHandler = this.submitHandler.bind(this)
+        this.firstNameValidation = this.firstNameValidation.bind(this)
+        this.lastNameValidation = this.lastNameValidation.bind(this)
+        this.emailValidation = this.emailValidation.bind(this)
+
     }
 
-    firstNameHandler (event) {
-        this.setState({
-            firstNameData : event.nativeEvent.target.value
-        })
-    }
-
-    lastNameHandler (event) {
-        this.setState({
-            lastNameData : event.nativeEvent.target.value
-        })
-    }
-
-    emailHandler (event) {
-        this.setState({
-            emailData : event.nativeEvent.target.value
-        })
-    }
-
-    submitHandler (event) {
+    submitHandler(event) {
         event.preventDefault()
-        console.log(event)
+
         this.setState({
-            submitted:true 
+            submitted: true
         })
-        this.state.firstNameData && this.state.lastNameData && this.state.emailData && this.setState({
-            allValid : true 
-        })
-    }
 
+        if (this.state.firstNameData.length !== 0 && this.state.lastNameData.length !== 0 && this.state.emailData.length !== 0) {
+            this.setState({
+                allValid: true
+            })
 
-
-
-    render() {
-
-        if(this.state.allValid){
             setTimeout(() => {
                 this.setState({
-                    allValid:false
+                    allValid: false
                 })
-            },3000);
+            }, 3000);
         }
 
+        console.log(event);
+    }
+
+    firstNameValidation(event) {
+        this.setState({
+            firstNameData: event.target.value
+        })
+    }
+    lastNameValidation(event) {
+        this.setState({
+            lastNameData: event.target.value
+        })
+    }
+    emailValidation(event) {
+        this.setState({
+            emailData: event.target.value
+        })
+    }
+
+    render() {
         return (
             <div className="form-container">
-                <form className="register-form" autoComplete="off" onSubmit={(event) => this.submitHandler(event)}>
+                <form className="register-form" onSubmit={this.submitHandler} autoComplete="off">
                     {/* Uncomment the next line to show the success message */}
-                    {this.state.allValid && <div className="success-message">Success! Thank you for registering</div>}
+                    {this.state.submitted && this.state.allValid && (
+                        <div className="success-message">Success! Thank you for registering</div>
+                    )}
                     <input
                         id="first-name"
+                        onChange={this.firstNameValidation}
+                        value={this.state.firstNameData}
                         className="form-field"
                         type="text"
                         placeholder="First Name"
                         name="firstName"
-                        value={this.state.firstNameData}
-                        onChange={(event) => this.firstNameHandler(event)}
-
                     />
                     {/* Uncomment the next line to show the error message */}
-                    {(this.state.submitted && !this.state.firstNameData) && <span id="first-name-error">Please enter a first name</span>}
+                    {this.state.submitted && this.state.firstNameData.length === 0 && (
+                        <span id="first-name-error">Please enter a first name</span>
+                    )}
                     <input
                         id="last-name"
+                        onChange={this.lastNameValidation}
+                        value={this.state.lastNameData}
                         className="form-field"
                         type="text"
                         placeholder="Last Name"
                         name="lastName"
-                        value={this.state.lastNameData}
-                        onChange={(event) => this.lastNameHandler(event)}
-
                     />
                     {/* Uncomment the next line to show the error message */}
-                    {(this.state.submitted && !this.state.lastNameData) && <span id="last-name-error">Please enter a last name</span>}
+                    {this.state.submitted && this.state.lastNameData.length === 0 && (
+
+                        <span id="last-name-error">Please enter a last name</span>
+                    )}
                     <input
                         id="email"
+                        onChange={this.emailValidation}
+                        value={this.state.emailData}
                         className="form-field"
                         type="text"
                         placeholder="Email"
                         name="email"
-                        value={this.state.emailData}
-                        onChange={(event) => this.emailHandler(event)}
-
-
                     />
                     {/* Uncomment the next line to show the error message */}
-                    {(this.state.submitted && !this.state.emailData) &&<span id="email-error">Please enter an email address</span>}
+                    {this.state.submitted && this.state.emailData.length === 0 && (
+                        <span id="email-error">Please enter an email address</span>
+                    )}
                     <button className="form-field" type="submit">
                         Register
                     </button>
