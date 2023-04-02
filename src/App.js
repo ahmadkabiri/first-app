ce2492e4b4793365dfdb70144e67ccfa5ee2fd91
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-03 00:44:08 +0430

    proTodoListWithReact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Shop from './components/Shop/Shop'
+import TodoList from './components/TodoList/TodoList'
 import './App.css'
 
 export default class App extends Component {
@@ -7,7 +7,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <Shop />
+                <TodoList></TodoList>
             </div>
         )
     }
