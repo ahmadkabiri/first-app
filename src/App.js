684e4ecd6e90aa5e7edbaffd51f927d88f99c08d
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-05 14:58:28 +0430

    todo list refactured to functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
-import  Alert  from 'react-bootstrap/Alert'
-import  Button  from 'react-bootstrap/Button'
+import React from 'react'
+import TodoList from './components/TodoList/TodoList'
+import './App.css'
 
+export default function App() {
 
-export default class App extends Component {
-
-    render() {
+    
         return (
             <div>
-                <h1>Hello SabzLearn</h1>
-                <Alert variant='secondary'> Your Wellcome <Alert.Link>Panel</Alert.Link> </Alert>
-                <Button variant='danger'> Cancel</Button>
+                <TodoList></TodoList>
             </div>
         )
-    }
+    
 }
---
+++ src/components/TodoList/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    
+        return (
+
+            <header>
+                <h1>SabzLearn Todo List</h1>
+            </header>
+
+        )
+    
+}
