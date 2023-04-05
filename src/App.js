e32c4a31908f8e4caa116d8215d3f6e51a4b86b6
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-05 23:29:49 +0430

    mount update unmount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import Shop from './components/Shop/Shop'
 import './App.css'
+import User from "./User"
 
 export default function App() {
 
-    
+    const [users, setUsers] = useState([
+        {id:1 , name : "amin"},
+        {id:2 , name : "amir"},
+        {id:3 , name : "ali"},
+        {id:4 , name : "babak"},
+
+    ]) ;
+
+    const removeHandler = (userID) => {
+        setUsers(prevUsers => {
+            let newUsers = prevUsers.filter(user=>{
+                return user.id !== userID
+            })
+            return newUsers
+        })
+    }
+
+
+  
+    useEffect(()=>{
+        console.log("component mount")
+    },[]) 
+
+    useEffect(()=>{
+        console.log("component updated")
+    },[users])
+   
+
         return (
             <div>
-                <Shop />
+                <ul>
+                   {users.map(user =>{
+                    return <User key={user.id} {...user} onRemove={removeHandler}></User>
+                   })} 
+                </ul>
             </div>
         )
     
