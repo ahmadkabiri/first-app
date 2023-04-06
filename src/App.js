19c80baf0c1630ba794521fc7a7a2cb47bc065e2
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-06 18:18:16 +0430

    BrowserRouter-Routes-Route-Link

--- src/App.js
+++ src/App.js
@@ -1,86 +1,22 @@
 import React ,{useState,useEffect} from 'react'
-import { Typography } from '@mui/material'
-import Container from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {Routes,Route} from 'react-router-dom'
+import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
+import Products from './component/Products'
+
 
 export default function App() {
-
-
-    const [columns , setColumns] = useState([
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          renderCell: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-        {
-            field: 'action',
-            headerName: 'Actions',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            renderCell: (params) =>(
-            <div style={{
-                width:"100%" , 
-                display : "felx" ,
-                justifyContent : "space-evenly",
-                 }}>
-            <EditIcon></EditIcon>
-            <DeleteIcon></DeleteIcon>
-
-            </div>
-            )
-        },
-      ]);
-
-      const [rows, setRows] = useState([
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ])
-
-
         return (
-         <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize = {10}
-        rowsPerPageOptions = {[5]} 
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </div>
+          <>
+          <Routes>
+            <Route path='/' element={<h1>I am main route</h1>}></Route>
+            <Route path='/home' element={<Home></Home>}></Route>
+            <Route path='/about' element={<About></About>}></Route>
+            <Route path='/contact' element={<Contact></Contact>}></Route>
+            <Route path='/products' element={<Products></Products>}></Route>
+
+          </Routes>
+          </>
         )
 }
