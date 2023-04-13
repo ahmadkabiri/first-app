7ac92cedf06830f766c97d836a66381e7cdad4ec
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-13 20:29:31 +0430

    fixed changed to sticky

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
+import LineStyleIcon from "@mui/icons-material/LineStyle";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import {
-  LineStyleIcon,
-  TimelineIcon,
-  TrendingUpIcon,
+    // LineStyleIcon,
+    // TimelineIcon,
+    // TrendingUpIcon ,
   PermIdentity,
   Storefront,
   AttachMoney,
@@ -22,10 +26,12 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
+            <Link to='/' className="link">
             <li className="sidebarListItem active">
               <LineStyleIcon className="sidebarIcon"></LineStyleIcon>
               Home
             </li>
+            </Link>
             <li className="sidebarListItem">
               <TimelineIcon></TimelineIcon>
               Analytics
@@ -39,18 +45,24 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
+            <Link to='/users' className="link">
             <li className="sidebarListItem">
               <PermIdentity className="sidebarIcon"></PermIdentity>
               Users
             </li>
+            </Link>
+            <Link to='/newUser' className="link">
             <li className="sidebarListItem">
               <PermIdentity></PermIdentity>
               New User
             </li>
+            </Link>
+            <Link to='/products' className="link">
             <li className="sidebarListItem">
               <Storefront></Storefront>
               Products
             </li>
+            </Link>
             <li className="sidebarListItem">
               <AttachMoney></AttachMoney>
               Transactions
@@ -83,6 +95,7 @@ export default function Sidebar() {
           <ul className="sidebarList">
             <li className="sidebarListItem">
               <WorkOutline className="sidebarIcon"></WorkOutline>
+              Manage
             </li>
             <li className="sidebarListItem">
               <TimelineIcon></TimelineIcon>
