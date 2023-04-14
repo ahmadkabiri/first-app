5d5107cf49104122efcb275b924e85bbb6ae53ed
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-14 12:32:04 +0430

    Install rechart

---
+++ src/datas.js
@@ -0,0 +1,17 @@
+let xAxisData = [
+    {"name":'Jan', "Sale":112_000},
+    {"name":'Feb', "Sale":99_000},
+    {"name":'Mar', "Sale":12_000},
+    {"name":'Apr', "Sale":99_000},
+    {"name":'May', "Sale":54_000},
+    {"name":'Jun', "Sale":85_000},
+    {"name":'Jul', "Sale":34_000},
+    {"name":'Agu', "Sale":18_000},
+    {"name":'Sep', "Sale":15_000},
+    {"name":'Oct', "Sale":73_000},
+    {"name":'Nov', "Sale":12_000},
+    {"name":'Dec', "Sale":97_000}
+]
+
+
+export {xAxisData}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Features from './../../components/features/Features';
+import './Home.css'
+import Chart from "../../components/chart/Chart";
+import { xAxisData } from "../../datas";
 
 export default function Home() {
   return (
-    <Features></Features>
+    <div className="home">
+      <Features></Features>
+      <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale"></Chart>
+    </div>
   )
 }
