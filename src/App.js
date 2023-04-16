dbd4a378101994eafb7e053aeb615533fa7eeee8
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-16 23:17:48 +0430

    useMemo example

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import { useState, useEffect, useMemo } from "react";
 // import "./App.css";
 export default function App() {
-  const [firstCount, setFirstCount] = useState(0);
-  const [secondCount, setSecondCount] = useState(0);
-  const [text, setText] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
 
-  const firstCounterHandler = () => {
-    setFirstCount((prevCount) => prevCount + 1);
+  const addNewNote = () => {
+    let newNoteObj = {
+      id: notes.length + 1,
+      title: newNote,
+    };
+
+    setNotes((prevNotes) => [...prevNotes, newNoteObj]);
+    setNewNote('')
   };
 
-  const secondCounterHandler = () => {
-    setSecondCount((prevCount) => prevCount + 1);
-  };
 
-  const isEven = useMemo(() => {
-    let index = 0;
-    while (index < 2000000000) {
-      index++;
-    }
-    return firstCount % 2 === 0;
-  }, [firstCount]);
 
   return (
     <div>
-      <button onClick={firstCounterHandler}>
-        Add First Counter : {firstCount}
-      </button>
-      {isEven ? "Even" : "Odd"}
-      <br />
-      <button onClick={secondCounterHandler}>
-        Add Second Counter : {secondCount}
-      </button>
+      <input
+        type="text"
+        value={newNote}
+        onChange={(event) => setNewNote(event.target.value)}
+      />
+      <button onClick={addNewNote}>Add New Note</button>
+      <hr />
+      <h3>All Notes</h3>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            {note.id} - {note.title}
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <h3>Js Included Note</h3>
+      {useMemo(()=>{
+        return notes.filter(note => note.title.toLowerCase().includes("js")).map(note=> (
+          <li>{note.id} - {note.title}</li>
+        ))
+      },[notes])}
     </div>
   );
 }
