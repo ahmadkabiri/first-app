e1c9596c6ccd68eb2fba6ee46341a27233b70c32
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-18 00:50:35 +0430

    useRef

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
-import { useState, useEffect, useMemo ,memo, useCallback } from "react";
+import { useState, useRef, useEffect } from "react";
 // import "./App.css";
 export default function App() {
-  const [counter, setCounter] = useState(0);
+  const userNameInputRef = useRef();
 
-  const [title, setTitle] = useState("");
+  useEffect(() => {
+    console.log(userNameInputRef);
+    userNameInputRef.current.focus();
+  }, []);
 
-  console.log("App run")
-
-  const addCounter = useCallback(() => {
-    setCounter((prevCount) => prevCount + 1);
-  },[counter]);
-
-  const minusCounter =useCallback(() => {
-    setCounter((prevCount) => prevCount - 1);
-  },[counter]);
+  const addNewValue = () => {
+    userNameInputRef.current.value = "Ahmad Kabiri";
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
-      />
-      <Title title={title}></Title>
-      <h3>{counter}</h3>
-      <Buttons add={addCounter} minus={minusCounter}></Buttons>
+      <input type="text" placeholder="UserName" ref={userNameInputRef} />
+      <button onClick={addNewValue}>Add New Value</button>
     </div>
   );
 }
-
-const Title =memo(({ title }) => {
-  console.log("title run");
-  return <h3>{title}</h3>;
-});
-
-const Buttons = memo(({ add, minus }) => {
-  console.log("buttons run");
-
-  return (
-    <>
-      <button onClick={add}>Add</button>
-      <button onClick={minus}>Minus</button>
-    </>
-  );
-});
