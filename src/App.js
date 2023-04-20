f87bef0e377efd77262aba3cba2412e2a4ae9194
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-20 19:41:04 +0430

    ToggleNoteList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useState, useRef, useEffect } from "react";
-// import "./App.css";
-import Reactjs from "./components/courses/Reactjs";
-import Javascript from "./components/courses/Javascript";
+import "./App.css";
+import ToggleNoteList from "./components/ToggleNoteList";
+
+
 
 export default function App() {
   return (
-    <div>
-      <Javascript></Javascript>
-      <hr />
-      <Reactjs></Reactjs>
+    <div className="app">
+      <div className="container">
+        <ToggleNoteList></ToggleNoteList>
+      </div>
     </div>
   );
 }
