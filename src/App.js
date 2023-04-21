64fd7e64e6064c4198176b0e144dd40dd22624a9
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-21 12:04:32 +0430

    exp-of-acc-project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { useState, useRef, useEffect } from "react";
-import "./App.css";
-import ToggleNoteList from "./components/ToggleNoteList";
+import React, { useState } from 'react';
+import data from './data'
+import Question from './Components/Question';
+import './App.css'
+
+function App() {
+  
+  const [infos , setInfos] = useState(data)
 
 
-
-export default function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <ToggleNoteList></ToggleNoteList>
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {/* data mapping */}
+        </section>
+        {
+          infos.map(info => (
+            <Question key={info.id} {...info}></Question>
+          ))
+        }
       </div>
-    </div>
+    </main>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App' ;
 import 'bootstrap/dist/css/bootstrap.css'
-import Test from './Test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
