4cdda73f9635582adba9a9c7156dd9e0618a252c
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-23 14:28:38 +0430

    profiler

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
-import data from './data'
-import Question from './Components/Question';
-import './App.css'
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useLayoutEffect,
+  Profiler,
+} from "react";
+import "./App.css";
+import User from "./Components/User";
 
 function App() {
-  
-  const [infos , setInfos] = useState(data)
-
+  const [text, setText] = useState("");
 
   return (
-    <main>
-      <div className='container'>
-        <h3>questions and answers about login</h3>
-        <section className='info'>
-          {/* data mapping */}
-        </section>
-        {
-          infos.map(info => (
-            <Question key={info.id} {...info}></Question>
-          ))
-        }
-      </div>
-    </main>
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+      <Message></Message>
+      <User></User>
+    </div>
   );
 }
+const Message = () => {
+  const [title, setTitle] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+    </div>
+  );
+};
 
 export default App;
