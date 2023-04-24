3f757d9fa725cd185df1c953c367e6032e7c271f
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-04-25 01:51:05 +0430

    UserCart

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from "./Components/Navbar/Navbar";
 import ProductsSection from "./Components/ProductsSection/ProductsSection";
 import products from "./data/products";
 import Toast from "./Components/Toast/Toast";
 import Cart from "./Components/Cart/Cart";
-function App() {
+import productsContext from "./Contexts/ProductsContext";
 
-  const [allProducts,setAllProducts] =  useState(products)
+function App() {
+  const [allProducts, setAllProducts] = useState(products);
+  const [userCart, setUserCart] = useState([]);
+  const [isShowToast,setIsShowToast] = useState(false)
+  const [isShowCart , setIsShowCart] = useState(false)
 
   return (
     <div>
-      <Navbar></Navbar>
-      <main className="pb-5">
-        <div className="container">
-          <h1 className="text-center main-title">All Products</h1>
-          {
-            allProducts.map(product=>(
-              <ProductsSection {...product}></ProductsSection>
-            ))
-          }
-
-        </div>
-      </main>
-      <Toast></Toast>
-      <Cart/>
+      <productsContext.Provider
+        value={{
+          allProducts,
+          userCart,
+          setUserCart,
+          isShowToast,
+          setIsShowToast,
+          isShowCart,
+          setIsShowCart
+        }}
+      >
+        <Navbar></Navbar>
+        <main className="pb-5">
+          <div className="container">
+            <h1 className="text-center main-title">All Products</h1>
+              <ProductsSection></ProductsSection>
+          </div>
+        </main>
+        <Toast></Toast>
+        <Cart />
+      </productsContext.Provider>
     </div>
   );
 }
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,40 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {BsBag} from 'react-icons/bs'
 import { AiOutlineClose } from 'react-icons/ai'
 import './Cart.css'
+import productsContext from '../../Contexts/ProductsContext'
+
 
 function Cart() {
+
+   const contextData = useContext(productsContext)
+
   return (
-    <aside className='bag-sidebar active'> {/* add actice class to show bag sidebar */}
+    <aside className={`${contextData.isShowCart ? "active" : ''} bag-sidebar`}> {/* add actice class to show bag sidebar */}
     <h3 className="bag-title">
         <span>
             <BsBag></BsBag>
             Bag
         </span>
         <span>
-            <AiOutlineClose className='close-icon'></AiOutlineClose>
+            <AiOutlineClose 
+            className='close-icon'
+            onClick={()=>{
+                contextData.setIsShowCart(false)
+            }}
+            />
         </span>
     </h3>
 
     <div className="row bag-wrapper">
-        <div className="col-12 mt-5">
+       {
+        contextData.userCart.map(product => (
+            <div className="col-12 mt-5" key={product.id}>
             <div className="card py-3 px-3">
                 <div className='col-12 text-center'>
                     <img src="/images/item-1.jpeg" alt="ProductImage" className='cart-img-top w-75' />
 
                 </div>
                 <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                    <p className="card-text">lsdkdfj</p>
-                    <p className="price">120</p>
+                    <p className="card-text">{product.title}</p>
+                    <p className="price">{product.price}$</p>
                     <br />
                     <a href="#" className='btn btn-danger'>Buy</a>
                     <a href="#" className='btn btn-outline-dark mt-3 text-capitalize'>
                         more information
                     </a>
-                    <p className='number'>1</p>
+                    <p className='number'>{product.count}</p>
                 </div>
             </div>
         </div>
+        ))
+       }
     </div>
     </aside>
   )
