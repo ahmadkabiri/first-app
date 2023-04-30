aebe2b0ea2e13f3f250c6f08f5224ca27eadefb2
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-05-01 00:46:41 +0430

    modal bugfix

--- src/Components/DeleteModal/DeleteModal.js
+++ src/Components/DeleteModal/DeleteModal.js
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom";
 
 export default function DeleteModal() {
   return ReactDOM.createPortal(
+    <div className='modal-parent active'>
     <div className="delete-modal">
       <h1>آیا از حذف اطمینان دارید؟</h1>
       <div className="delete-modal-btns">
         <button className="delete-btn delete-modal-accept-btn">بله</button>
         <button className="delete-btn delete-modal-reject-btn">خیر</button>
       </div>
-    </div>,
+    </div>
+    </div>
+    ,
 
     document.getElementById("modals-parent")
   );
