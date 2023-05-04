610d46a578b4aecc41b5d4dc1e3c8b026ad447f1
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-05-04 21:55:23 +0430

    backend add to project

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./Products.css"
-import Errorbox from '../Errorbox/Errorbox'
 import ProductsTable from '../ProductsTable/ProductsTable'
 import AddNewProduct from '../AddNewProduct/AddNewProduct'
 
@@ -9,7 +8,6 @@ export default function Products() {
   return (
     <>
     <AddNewProduct/>
-    <Errorbox msg="هیچ محصولی یافت نشد"/>
     <ProductsTable/>
 
     </>
--- src/Components/ProductsTable/ProductsTable.js
+++ src/Components/ProductsTable/ProductsTable.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsTable.css";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import DetailsModal from "../DetailsModal/DetailsModal";
 import EditModal from "../EditModal/EditModal";
+
+import Errorbox from "../Errorbox/Errorbox";
+
 export default function ProductsTable() {
   const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
   const [isShowDetailsModal, setIsShowDetailsModal] = useState(false);
   const [isShowEditModal, setIsShowEditModal] = useState(false);
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/products/")
+      .then((res) => res.json())
+      .then((products) => setAllProducts(products));
+  }, []);
 
   const deleteModalCancelAction = () => {
     console.log("مدال کنسل شد");
@@ -23,12 +33,14 @@ export default function ProductsTable() {
   };
 
   const updateProductInfos = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     console.log("محصول ویرایش شد");
   };
 
   return (
     <>
+    {
+      allProducts.length ? (
       <table className="products-table">
         <thead>
           <tr className="products-table-heading-tr">
@@ -38,41 +50,46 @@ export default function ProductsTable() {
             <th>موجودی</th>
           </tr>
         </thead>
+
         <tbody>
-          <tr className="products-table-tr">
-            <td>
-              <img
-                src="/images/item-1.jpeg"
-                alt="oilImage"
-                className="products-table-img"
-              />
-            </td>
-            <td>کیک</td>
-            <td>92000</td>
-            <td>2</td>
-            <td>
-              <button
-                className="products-table-btn"
-                onClick={() => setIsShowDetailsModal(true)}
-              >
-                جزییات
-              </button>
-              <button
-                className="products-table-btn"
-                onClick={() => setIsShowDeleteModal(true)}
-              >
-                حذف
-              </button>
-              <button
-                className="products-table-btn"
-                onClick={() => setIsShowEditModal(true)}
-              >
-                ویرایش
-              </button>
-            </td>
-          </tr>
+          {allProducts.map((product) => (
+            <tr key={product.id} className="products-table-tr">
+              <td>
+                <img
+                  src={product.img}
+                  alt="oilImage"
+                  className="products-table-img"
+                />
+              </td>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td>{product.count}</td>
+              <td>
+                <button
+                  className="products-table-btn"
+                  onClick={() => setIsShowDetailsModal(true)}
+                >
+                  جزییات
+                </button>
+                <button
+                  className="products-table-btn"
+                  onClick={() => setIsShowDeleteModal(true)}
+                >
+                  حذف
+                </button>
+                <button
+                  className="products-table-btn"
+                  onClick={() => setIsShowEditModal(true)}
+                >
+                  ویرایش
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+      ) : (<Errorbox msg="هیچ محصولی یافت نشد" />)
+    }
       {isShowDeleteModal && (
         <DeleteModal
           submit={deleteModalSubmitAction}
